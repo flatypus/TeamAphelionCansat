533c5a8ba787d1cefeab55cff9b0c5459f2132ea
flatypus [mac] <yhc3141@gmail.com>
2024-04-06 20:31:32 -0700

    fix: setup mission control

---
+++ mission_control/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Eric Hamber Secondary School - Team Aphelion",
+  description:
+    "Team Aphelion's mission control dashboard for the 2024 CanSat competition",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ mission_control/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="bg-bg-gray min-h-screen w-full">Hello!</div>;
+}
